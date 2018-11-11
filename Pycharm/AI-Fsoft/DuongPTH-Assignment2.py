44c2df3871b695bca4a4a5f0efcb322aa9c11100
Dragneol <dragneol@outlook.com.vn>
2018-11-11 17:46:14 +0700

    finish week 6

--- Pycharm/AI-Fsoft/DuongPTH-Assignment2.py
+++ Pycharm/AI-Fsoft/DuongPTH-Assignment2.py
@@ -1,9 +1,8 @@
-from sklearn.metrics import r2_score
-from sklearn import linear_model
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
-import requests
+import pandas as pd
+from sklearn import linear_model
+from sklearn.metrics import r2_score
 
 link = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/housing.data'
 
