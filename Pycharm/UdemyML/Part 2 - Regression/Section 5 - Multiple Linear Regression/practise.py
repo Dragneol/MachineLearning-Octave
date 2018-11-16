f084ea7f4f88abcca579f6d6782ae656f60c8284
DuongPTH <duongpth.developer@gmail.com>
2018-11-16 17:00:38 +0700

    review for homework

--- Pycharm/UdemyML/Part 2 - Regression/Section 5 - Multiple Linear Regression/practise.py
+++ Pycharm/UdemyML/Part 2 - Regression/Section 5 - Multiple Linear Regression/practise.py
@@ -1,7 +1,10 @@
 # Multiple Linear Regression
 
 # Import library
+import numpy as np
+import matplotlib
 import pandas as pd
+import statsmodels.formula.api as sm
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
@@ -22,7 +25,7 @@ X = onehotencoder.fit_transform(X).toarray()
 # Avoid the Dummy Variable Trap
 
 # Slip train set and test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Fitting Multiple Linear Regression to the Training Set
 regressor = LinearRegression()
@@ -32,3 +35,5 @@ regressor.fit(X_train, y_train)
 y_pred = regressor.predict(X_test)
 
 # Building the optimal model using Backward Elimination
+X = np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
+# X_opt =
